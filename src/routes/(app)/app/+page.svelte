e10706f02ada57cfcacf9648153b7d6607aaf60d
rishigithub9440 <143729381+rishigithub9440@users.noreply.github.com>
2023-09-02 15:05:41 +0530

    Update +page.svelte

--- src/routes/(app)/app/+page.svelte
+++ src/routes/(app)/app/+page.svelte
@@ -590,7 +590,7 @@
 
 <!-- Move to component -->
 
-{#if !userInfo.trained && !userInfo.in_training}
+{#if userInfo.trained && !userInfo.in_training}
 <div class="form-control w-full max-w-xs">
   <label class="label">
     <span class="label-text text-inherit">Choose the style</span>
