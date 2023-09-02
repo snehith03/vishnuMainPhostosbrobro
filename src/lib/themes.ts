cbabef6101a639d9d1fd8001249cc3a50254306b
rishigithub9440 <143729381+rishigithub9440@users.noreply.github.com>
2023-09-02 15:22:29 +0530

    Update themes.ts

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -17,7 +17,7 @@ export const prompts: Theme[] = [
 		name: 'Jedi',
 		instance_classes: [],
 		prompt:
-			'closeup portrait of @me as a handsome cyberpunk, dark and gritty, highly detailed, looking straight to camera, retro-futuristic style, neon lighting, cyberpunk city in the background, art by wlop, greg rutkowski, and charlie bowater, 8 k resolution, ultra-realistic, octane render, unreal engine.'
+			'closeup portrait of @me as a beautiful cyberpunk, dark and gritty, highly detailed, looking straight to camera, retro-futuristic style, neon lighting, cyberpunk city in the background, art by wlop, greg rutkowski, and charlie bowater, 8 k resolution, ultra-realistic, octane render, unreal engine.'
 	}
 	
 ];
